cb7d82bb14c3069ff19e21a56b8e4d4fb199dbeb
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2022-12-29 18:47:33 +0300

    Update restart_prometheus.sh
    Changed the way exited containers remove

--- restart_prometheus.sh
+++ restart_prometheus.sh
@@ -1,2 +1,3 @@
 docker restart $(docker ps | grep prom/prometheus | grep -oE "^[0-9a-zA-Z]+")
-docker service update monitoring_prometheus && docker container prune
+# docker service update monitoring_prometheus && docker container prune
+docker rm $(docker ps --filter status=exited -q)
