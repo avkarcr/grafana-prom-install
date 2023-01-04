87dc57a9f441974be6c9dd46022ea25c0456f104
Alexey Karaev <99138558+avkarcr@users.noreply.github.com>
2023-01-04 23:28:11 +0300

    Update restart_prometheus.sh
    Added sleep

--- restart_prometheus.sh
+++ restart_prometheus.sh
@@ -1,3 +1,7 @@
 docker restart $(docker ps | grep prom/prometheus | grep -oE "^[0-9a-zA-Z]+")
 # docker service update monitoring_prometheus && docker container prune
+echo
+echo "Waiting for the docker containers to run..."
+sleep 10
 docker rm $(docker ps --filter status=exited -q)
+echo "All is done! You can check if no container is stopped by typing \"docker ps -a | grep -i exited\""
